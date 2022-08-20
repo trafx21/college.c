b1511f484ede10df811a4019c1c4234272a723cb
Washiul <washiul92@gmail.com>
2022-08-20 19:36:37 +0530

    initial commit

---
+++ trin.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+float base, height;
+printf ("enter the height of the tringle");
+scanf("%f", &height);
+printf("enter the breath of tringle");
+scanf("%f",&base);
+printf("area of tringle is : %f",  base * height /2);
+return 0;
+
+}
