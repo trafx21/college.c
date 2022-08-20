0212dd1ccf9fc709e44f441cb5c72257884fbc80
Washiul <washiul92@gmail.com>
2022-08-20 21:18:03 +0530

    first commit

---
+++ sizes_types.c
@@ -0,0 +1,18 @@
+/*
+    program to check sizes of each pri itive datatypes
+*/
+
+
+
+#include<stdio.h>
+
+
+int main()
+{
+    printf(" Size of Int in bytes: %d \n", sizeof(int));
+    printf(" Size of Char in bytes: %d \n", sizeof(char));
+    printf(" Size of float in bytes: %d \n", sizeof(float));
+    printf(" Size of double in bytes: %d \n", sizeof(double));
+
+    return 0;
+}
